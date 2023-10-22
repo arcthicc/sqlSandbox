19286c06170dbb29b2d519a561fd07cf4bf562fd
arcthicc <jas.kanthan@gmail.com>
2023-10-21 20:35:40 -0400

    EVERYTHING WORKS ABAHAHAHHAHAHHA

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8080; // Change this to your desired port
 
@@ -10,6 +11,11 @@ app.get('/', (req, res) => {
 app.use(express.static("public"))
 
 app.use(express.json());
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 // Route to get all data
 app.get('/data/getalldata', (req, res) => {
@@ -31,7 +37,8 @@ app.get('/data/getspecificdata/:id', async (req, res) => {
 // Route to create new data
 app.post('/data/createdata', (req, res) => {
     try {
-        const item = req.body; // Data to create
+        const item = req.body.input; // Data to create
+        console.log(item);
         // Process and save the data using the createData function or your logic
         const newItem = createData(item);
         // Send a success response
